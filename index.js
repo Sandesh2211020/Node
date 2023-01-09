82cc4bc2709510afd95cb576abba73077fa9aa1c
Sandesh2211020 <sandesh2211020@iimscollege.edu.np>
2023-01-09 08:46:38 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let citrus = fruits.slice(1,3);
+console.log(fruits); //["Banana", "Orange", "Lemon", "Apple", "Mango"]
+console.log(citrus); // [ 'Orange', 'Lemon' ]
