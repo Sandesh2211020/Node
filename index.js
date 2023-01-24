f2db5aa0caca27d4ca172ef0fd03dc8e7c346b1a
Sandesh2211020 <sandesh2211020@iimscollege.edu.np>
2023-01-24 08:39:04 +0545

    Promise part 2

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-//-----------------Promises----------------------
+//----------------------Promise part 2-------------------------------------
 
-// function examplePromise (){
-//     return new Promise(function (resolve, reject){
-//         resolve("Hello World")
-//     });
-// }
-
-
-
-const examplePromise = () =>{
+let promise1 = () => {
     return new Promise((resolve, reject) => {
-        reject("Hello World")
-        
-    });
+        setTimeout(() => {
+            resolve("hello")
+        }, 1000)
+    })
 };
 
-examplePromise()
-.then(function(result){
-    console.log("Result is: ",result);
-})
-.catch((error) => {
-    console.log("Error: " ,error);
-})
-.finally(() => {
-    console.log("Runnnnnnn");
-})
+let promise2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("world")
+        }, 2000)
+    })
+};
+
+//---------------------Ez method---------------------------------
+// let(name, age) = {
+//     name = "Sandesh",
+//     age = 19,
+// }
+
+// let arr = ["hello", "world"]
+// let [first, second] = arr
+// console.log(first);
+
+
+// const arrayOfPromises = [promise1(), promise2()]
+// Promise.all(arrayOfPromises)
+// .then(([promise1Value, promise2Value])=> {
+//     console.log(promise1Value);
+//     console.log(promise2Value);
+// })
+
+//-------------------async and await----------------------
+
+const resultFunction = async () => {
+    try {
+        // let promise1Value = await promise1();
+        // let promise2Value = await promise2();               // 3 second complete time.
+        let [promise1Value, promise2Value] = await Promise.all([
+            promise1(),
+            promise2(),
+        ]); // complete in 2 second.
+
+        console.log(promise1Value, promise2Value);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+resultFunction();
+
+
+
+
+
